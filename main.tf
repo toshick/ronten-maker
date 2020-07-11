529c3e370df61377810bf6038a9a210a7cfd12e7
toshick <toshickjazz@mac.com>
2020-07-11 18:32:25 +0900

    feat: gcr.io/ronten-maker/ronten-maker

--- main.tf
+++ main.tf
@@ -1,7 +1,9 @@
 
 variable "docker_declaration" {
-  type    = string
-  default = "spec:\n  containers:\n    - name: ronron-name\n      image: 'gcr.io/ronten-maker/app3'\n      stdin: false\n      tty: false\n  restartPolicy: Always\n"
+  type = string
+  # gcr.io/ronten-maker/app3
+  # gcr.io/ronten-maker/ronten-maker
+  default = "spec:\n  containers:\n    - name: ronron-name\n      image: 'gcr.io/ronten-maker/ronten-maker'\n      stdin: false\n      tty: false\n  restartPolicy: Always\n"
 }
 
 
